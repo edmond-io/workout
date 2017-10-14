88fcb08d9fa1da3115c2da2407a664f05c1d3134
Edmond Lo <edmond1228@gmail.com>
2017-10-14 19:46:16 +0800

    Implement Mobile View for MuscleComponent

--- angular-src/src/app/component/exercise/exercise.component.ts
+++ angular-src/src/app/component/exercise/exercise.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, Input } from '@angular/core';
+import { Location } from '@angular/common';
+import { Exercise } from '../../model/exercise';
 
 @Component({
   selector: 'app-exercise',
@@ -6,12 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise.component.css']
 })
 export class ExerciseComponent implements OnInit {
+  @Input() private theExercise: Exercise;
 
-  constructor() {
-	  
+  constructor(private location: Location) {
   }
 
-  ngOnInit() {
+	ngOnInit() {
+		// this.route.params
+		// 	.switchMap((params: Params) => this.exerciseService.get(params['ex_id']))
+    //   .subscribe(vo => {
+		// 		this.theExercise = vo;
+  	// 	});
   }
 
+	back(){
+		this.location.back();
+	}
+
+  transform(field, suffix){
+		if (!this.theExercise || this.isEmpty(this.theExercise[field]))
+			return "";
+
+		var result = "";
+
+		switch (field){
+			case "reps":
+			case "sets":
+
+				if (!this.isEmpty(this.theExercise[field][0])
+						&& !this.isEmpty(this.theExercise[field][1]))
+					result = this.theExercise[field][0] + " - " + this.theExercise[field][1];
+
+				else if (!this.isEmpty(this.theExercise[field][0]))
+					result = this.theExercise[field][0];
+
+				break;
+		}
+
+		if (!suffix) {
+			return result;
+		} else {
+			return result + " " + suffix;
+		}
+	}
+
+	isEmpty(obj){
+		return (!obj);
+	}
 }
